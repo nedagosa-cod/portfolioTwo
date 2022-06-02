f17c709fd2c6c6aba532456c6e10032672a0a30d
nedagosa-cod <nestorcode4004@gmail.com>
2022-06-02 14:01:24 -0500

    se termina footer

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
+const overlay = document.getElementById('overlayForm');
+
 const toggleMenu = () => {
-
-        // document.getElementById("myNav").style.height = "100%";
-        document.querySelector('.toggle').classList.toggle('active');
-        document.querySelector('.overlay').classList.toggle('active');
-
-}
-
-
-
-
-const closeMenu = () => {
-    document.getElementById("myNav").style.height = "0%";
+    document.querySelector('.toggle').classList.toggle('active');
+    document.querySelector('.overlay').classList.toggle('active');
 }
 document.getElementById('toggle').addEventListener('click', toggleMenu);
+overlay.addEventListener('click', toggleMenu);

493d3c7005c34708a4ed5a153165b27c2a352e14
nedagosa-cod <nestorcode4004@gmail.com>
2022-06-02 00:00:06 -0500

    se configura maquetacion de about

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const toggleMenu = () => {
-    document.querySelector('.toggle').classList.toggle('active');
+
+        // document.getElementById("myNav").style.height = "100%";
+        document.querySelector('.toggle').classList.toggle('active');
+        document.querySelector('.overlay').classList.toggle('active');
+
 }
 
+
+
+
+const closeMenu = () => {
+    document.getElementById("myNav").style.height = "0%";
+}
 document.getElementById('toggle').addEventListener('click', toggleMenu);

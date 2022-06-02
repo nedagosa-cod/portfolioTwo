d3b5e512a9a92443a485d50b8063f52b0ad943d1
nedagosa-cod <nestorcode4004@gmail.com>
2022-06-02 17:55:01 -0500

    se agrega scroll reveal

---
+++ js/reveal.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal({
+    reset:true,
+})
+
+ScrollReveal().reveal('.main__imgbx', { duration: 1800, origin: 'left', distance: '200px' });
+ScrollReveal().reveal('.main__text', { duration: 1500, origin: 'bottom', distance: '-400px' });
+
+ScrollReveal().reveal('.class__article--p', { duration: 1000, origin: 'left', distance: '400px' });
+ScrollReveal().reveal('.about__imgbx', { duration: 1000, origin: 'right', distance: '400px' });
+
+ScrollReveal().reveal('.skills__cards', { duration: 2000, origin: 'bottom', distance: '300px' });
+ScrollReveal().reveal('.folios__cards', { duration: 2500});

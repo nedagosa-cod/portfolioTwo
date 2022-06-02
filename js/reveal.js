f6a954e9874b3d8d16361d23dcf2db5033e8d473
nedagosa-cod <nestorcode4004@gmail.com>
2022-06-02 18:38:32 -0500

    primera adaptacion responsive

--- js/reveal.js
+++ js/reveal.js
@@ -8,5 +8,5 @@ ScrollReveal().reveal('.main__text', { duration: 1500, origin: 'bottom', distanc
 ScrollReveal().reveal('.class__article--p', { duration: 1000, origin: 'left', distance: '400px' });
 ScrollReveal().reveal('.about__imgbx', { duration: 1000, origin: 'right', distance: '400px' });
 
-ScrollReveal().reveal('.skills__cards', { duration: 2000, origin: 'bottom', distance: '300px' });
+ScrollReveal().reveal('.skills__card', { duration: 2000, origin: 'bottom', distance: '300px' });
 ScrollReveal().reveal('.folios__cards', { duration: 2500});
